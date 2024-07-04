c3b9d7c4ed1c1236803eb266c8d19b70e9f62f1a
Muhammad Firdaus <uizwmuha@Astro.com.my>
2024-07-04 22:52:22 +0800

    Done

---
+++ check-8/cpu.sh
@@ -0,0 +1,75 @@
+{
+  "namespace": "kube-system",
+  "name": "calico-kube-controllers-75bdb5b75d-2b6mr",
+  "cpu": null
+}
+{
+  "namespace": "kube-system",
+  "name": "canal-q652m",
+  "cpu": null
+}
+{
+  "namespace": "kube-system",
+  "name": "canal-q652m",
+  "cpu": null
+}
+{
+  "namespace": "kube-system",
+  "name": "canal-wzjz6",
+  "cpu": null
+}
+{
+  "namespace": "kube-system",
+  "name": "canal-wzjz6",
+  "cpu": null
+}
+{
+  "namespace": "kube-system",
+  "name": "coredns-5c69dbb7bd-6xvhl",
+  "cpu": null
+}
+{
+  "namespace": "kube-system",
+  "name": "coredns-5c69dbb7bd-xfk7l",
+  "cpu": null
+}
+{
+  "namespace": "kube-system",
+  "name": "etcd-controlplane",
+  "cpu": null
+}
+{
+  "namespace": "kube-system",
+  "name": "kube-apiserver-controlplane",
+  "cpu": null
+}
+{
+  "namespace": "kube-system",
+  "name": "kube-controller-manager-controlplane",
+  "cpu": null
+}
+{
+  "namespace": "kube-system",
+  "name": "kube-proxy-dp5fn",
+  "cpu": null
+}
+{
+  "namespace": "kube-system",
+  "name": "kube-proxy-nhmtq",
+  "cpu": null
+}
+{
+  "namespace": "kube-system",
+  "name": "kube-scheduler-controlplane",
+  "cpu": null
+}
+{
+  "namespace": "local-path-storage",
+  "name": "local-path-provisioner-75655fcf79-xw9b6",
+  "cpu": null
+}
+{
+  "namespace": "qq2",
+  "name": "busybox",
+  "cpu": null
+}
